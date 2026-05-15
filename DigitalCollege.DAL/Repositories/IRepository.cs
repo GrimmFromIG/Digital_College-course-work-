5f16c1ac98e44fe84bebb47e8a49ce4fc39c721e
GrimmFromIG <gamerboy200715@gmail.com>
2026-05-16 02:05:11 +0300

    Repo

---
+++ DigitalCollege.DAL/Repositories/IRepository.cs
@@ -0,0 +1,14 @@
+using System.Linq.Expressions;
+
+namespace DigitalCollege.DAL.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        T GetById(int id);
+        IEnumerable<T> Find(Expression<Func<T, bool>> predicate);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(int id);
+    }
+}
---
+++ DigitalCollege.DAL/Repositories/Repository.cs
@@ -0,0 +1,42 @@
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace DigitalCollege.DAL.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected readonly DbContext _context;
+        protected readonly DbSet<T> _dbSet;
+
+        public Repository(DbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+        public IEnumerable<T> GetAll() => _dbSet.ToList();
+
+        public T GetById(int id) => _dbSet.Find(id);
+
+        public IEnumerable<T> Find(Expression<Func<T, bool>> predicate)
+        {
+            return _dbSet.Where(predicate).ToList();
+        }
+
+        public void Add(T entity) => _dbSet.Add(entity);
+
+        public void Update(T entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+        }
+
+        public void Delete(int id)
+        {
+            T entity = _dbSet.Find(id);
+            if (entity != null)
+            {
+                _dbSet.Remove(entity);
+            }
+        }
+    }
+}
