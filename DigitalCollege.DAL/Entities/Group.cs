728898d3c8f4a2cccbf57be55253faedec839adf
GrimmFromIG <gamerboy200715@gmail.com>
2026-05-15 15:22:14 +0300

    DAL

---
+++ DigitalCollege.DAL/Entities/Group.cs
@@ -0,0 +1,10 @@
+namespace DigitalCollege.DAL.Entities
+{
+    public class Group
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<Student> Students { get; set; }
+    }
+}
