067aa2098f5067076c6b062dd0e98e5b9e25a750
Nortover <staspushko7@gmail.com>
2026-05-25 17:23:24 +0300

    UI

--- DigitalCollege.UI/Program.cs
+++ DigitalCollege.UI/Program.cs
@@ -4,7 +4,7 @@ using DigitalCollege.UI.Services;
 using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<DigitalCollege.UI.App>("#app");//builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<DigitalCollege.UI.App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
