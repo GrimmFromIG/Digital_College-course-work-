b989a461c65e9068e336a1e2ecaed7d03c1736e9
Nortover <staspushko7@gmail.com>
2026-05-25 17:13:20 +0300

    UI

--- DigitalCollege.UI/Models/GroupModel.cs
+++ DigitalCollege.UI/Models/GroupModel.cs
@@ -7,7 +7,7 @@ namespace DigitalCollege.UI.Models
         public int Id { get; set; }
 
         [Required(ErrorMessage = "Назва групи обов'язкова для заповнення!")]
-        [StringLength(10, MinimumLength = 2, ErrorMessage = "Назва групи має бути від 2 до 10 символів.")]
+        [StringLength(20, MinimumLength = 2, ErrorMessage = "Назва групи має бути від 2 до 20 символів.")]
         public string Name { get; set; }
     }
 }
