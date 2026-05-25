f15a5e349998a65703a27cc38dba6b64535b155f
Nortover <staspushko7@gmail.com>
2026-05-25 17:22:40 +0300

    UI

--- DigitalCollege.UI/Program.cs
+++ DigitalCollege.UI/Program.cs
@@ -1,15 +1,18 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using DigitalCollege.UI;
 using DigitalCollege.UI.Services;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<DigitalCollege.UI.App>("#app");//builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7238") });//https://localhost:5085
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7238") });
 
 builder.Services.AddScoped<ApiClient>();
 builder.Services.AddSingleton<AuthService>();
+
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
